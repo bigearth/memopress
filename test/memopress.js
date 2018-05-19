b0fe5e5c59ce88627a0a1e142d45453dedf87b04
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-19 11:55:10 +0800

    Tests.

--- test/memopress.js
+++ test/memopress.js
@@ -5,8 +5,24 @@ let memopress = require('../index');
 
 describe('#memopress', () => {
   describe('#memo', () => {
+    describe('#encode', () => {
+      fixtures.memo.encode.forEach((fixture) => {
+        it(`should encode memo`, () => {
+          let data;
+          if(fixture.prefix === '0x6d03') {
+            data = memopress.encode(fixture.prefix, {txHash: fixture.data.txHash, message: fixture.data.message});
+          } else if(fixture.prefix === '0x6d0C') {
+            data = memopress.encode(fixture.prefix, {topic: fixture.data.topic, message: fixture.data.message});
+          } else {
+            data = memopress.encode(fixture.prefix, fixture.data);
+          }
+          assert.equal(data.toString('hex'), fixture.hex);
+        });
+      });
+    });
+
     describe('#decode', () => {
-      fixtures.memo.forEach((fixture) => {
+      fixtures.memo.decode.forEach((fixture) => {
         it(`should decode ${fixture.asm}`, () => {
           let data = memopress.decode(fixture.asm);
           assert.deepEqual(data, fixture.data);
@@ -16,8 +32,17 @@ describe('#memopress', () => {
   });
 
   describe('#blockpress', () => {
+    describe('#encode', () => {
+      // fixtures.blockpress.decode.forEach((fixture) => {
+      //   it(`should encode blockpress`, () => {
+      //     let data = memopress.decode(fixture.asm);
+      //     assert.deepEqual(data, fixture.data);
+      //   });
+      // });
+    });
+
     describe('#decode', () => {
-      fixtures.blockpress.forEach((fixture) => {
+      fixtures.blockpress.decode.forEach((fixture) => {
         it(`should decode ${fixture.asm}`, () => {
           let data = memopress.decode(fixture.asm);
           assert.deepEqual(data, fixture.data);
