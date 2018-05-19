0ea31e580ff96638dd107bedaa7ddb761c5405a2
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-19 12:07:04 +0800

    Tests

--- test/memopress.js
+++ test/memopress.js
@@ -33,12 +33,21 @@ describe('#memopress', () => {
 
   describe('#blockpress', () => {
     describe('#encode', () => {
-      // fixtures.blockpress.decode.forEach((fixture) => {
-      //   it(`should encode blockpress`, () => {
-      //     let data = memopress.decode(fixture.asm);
-      //     assert.deepEqual(data, fixture.data);
-      //   });
-      // });
+      fixtures.blockpress.encode.forEach((fixture) => {
+        it(`should encode blockpress`, () => {
+          let data;
+          if(fixture.prefix === '0x8d03') {
+            data = memopress.encode(fixture.prefix, {txHash: fixture.data.txHash, message: fixture.data.message});
+          } else if(fixture.prefix === '0x8d09') {
+            data = memopress.encode(fixture.prefix, {mediaType: fixture.data.mediaType, mediaPayload: fixture.data.mediaPayload, mediaText: fixture.data.mediaText});
+          } else if(fixture.prefix === '0x8d11') {
+            data = memopress.encode(fixture.prefix, {community: fixture.data.community, message: fixture.data.message});
+          } else {
+            data = memopress.encode(fixture.prefix, fixture.data);
+          }
+          assert.equal(data.toString('hex'), fixture.hex);
+        });
+      });
     });
 
     describe('#decode', () => {
