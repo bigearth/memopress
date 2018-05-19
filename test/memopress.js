3960862fc7fe81d9ac3094cb299fdab8fea154ac
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-19 08:08:50 +0800

    Tests and fixtures.

--- test/memopress.js
+++ test/memopress.js
@@ -1,14 +1,27 @@
 let fixtures = require('./fixtures/memopress.json')
 let chai = require('chai');
 let assert = require('assert');
-let memopress = require('memopress');
+let memopress = require('../index');
 
 describe('#memopress', () => {
   describe('#memo', () => {
-    fixtures.memo.forEach((fixture) => {
-      it(`should decode ${fixture.asm}`, () => {
-        let data = memopress.decode(fixture.asm);
-        assert.equal(data, fixture.data);
+    describe('#decode', () => {
+      fixtures.memo.forEach((fixture) => {
+        it(`should decode ${fixture.asm}`, () => {
+          let data = memopress.decode(fixture.asm);
+          assert.deepEqual(data, fixture.data);
+        });
+      });
+    });
+  });
+
+  describe('#blockpress', () => {
+    describe('#decode', () => {
+      fixtures.blockpress.forEach((fixture) => {
+        it(`should decode ${fixture.asm}`, () => {
+          let data = memopress.decode(fixture.asm);
+          assert.deepEqual(data, fixture.data);
+        });
       });
     });
   });
